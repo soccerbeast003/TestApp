adedf679f526eb8e6b978c54b04e87cc7ecde442
Derek Schreiner <soccerbeast003@gmail.com>
2013-03-29 17:42:51 -0400

    initial check

---
+++ src/com/example/TestApp/MyActivity.java
@@ -0,0 +1,31 @@
+package com.example.TestApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+        final Button button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new OnClickListener()
+        {
+            public void onClick(View v)
+            {
+
+                button.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
